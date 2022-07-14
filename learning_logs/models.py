6a07990622d6f6c65d6fe2bcbb116ce898eab725
Clayton <cdston@gmail.com>
2022-07-14 14:46:44 -0300

    Registrando Entry junto ao site de administração

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -14,7 +14,7 @@ class Topic(models.Model):
 
 """Algo especifico aprendido sobre um assunto"""
 class Entry(models.Model):
-    topic = models.ForeignKey(Topic)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now=True)
 
