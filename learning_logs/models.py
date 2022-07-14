e922957f424d1635088e595b823b8ec601b496f7
Clayton <cdston@gmail.com>
2022-07-14 12:42:16 -0300

    Definindo modelos

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+"""Um assunto sobre o qual o usuario esta aprendendo"""
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    #Devolve uma representação em string do modelo.
+    def __str__(self):
+        return self.text
